fca3a1cbcc2b2d3ac2d1f37c61ab261f4286fe02
Not Today <48966874+another-salad@users.noreply.github.com>
2023-10-23 20:37:35 +0100

    minecraft messager

---
+++ minecraft/app/helpers/get_servers.py
@@ -0,0 +1,26 @@
+"""Gets all the RCON servers from the JSON file"""
+
+from collections import namedtuple
+from json import loads
+from pathlib import Path
+import re
+
+
+Server = namedtuple("Server", ["host", "rconport", "datadir"])
+RCON_PW_RGX = r"rcon.password=(?P<pw>.*)"
+
+class NoPasswordSadness(Exception):
+    """Raised when we can't find the password in your RCON file"""
+
+def server_obj(dct) -> Server:
+    return Server(**dct)
+
+def get_servers() -> list[Server]:
+    return loads(Path(Path(__file__).parent, "servers.json").read_bytes(), object_hook=server_obj)
+
+def get_rcon_pw_from_prop_file(absolute_parent_path: str) -> str:
+    prop_file_contents = Path(absolute_parent_path, "server.properties").absolute().read_text()
+    res = re.search(RCON_PW_RGX, prop_file_contents)
+    if res:
+        return res.group("pw")
+    raise NoPasswordSadness("RCON password not found is server.properties")
---
+++ minecraft/app/helpers/msg.py
@@ -0,0 +1,38 @@
+"""Contains all the acceptable messages. I don't want to parse user input (ew)."""
+
+from datetime import datetime
+
+from mcipc.rcon.je import Client
+
+from get_servers import get_servers, get_rcon_pw_from_prop_file
+
+class InvaldMsg(Exception):
+    """Raised when an unknown message is requested"""
+
+def get_time() -> datetime:
+    return datetime.now().strftime('%H:%M:%S')
+
+def _time() -> str:
+    return f"The time is currently {get_time()}, are you still playing?"
+
+def _restart() -> str:
+    return f"Get to a safe place, the server will restart on the hour. The time is now: {get_time()}"
+
+def _players(client: Client) -> list:
+    player_info = client.list()
+    return f'Current Miners: {",".join(getattr(x, "name", "") for x in getattr(player_info, "players", []))}'
+
+ACCEPTABLE_MESSAGES = {
+    "time": _time,
+    "players": _players,
+    "restart": _restart
+}
+
+def send_msg(msg: str):
+    """Sends message via rcon to all servers in the server_conf.json file"""
+    server_msg = ACCEPTABLE_MESSAGES.get(msg, False)
+    if not server_msg:
+        raise InvaldMsg(f"Server message must be in approved list: {ACCEPTABLE_MESSAGES.keys()}")
+    for server in get_servers():
+        with Client(server.host, server.rconport, passwd=get_rcon_pw_from_prop_file(server.datadir)) as client:
+            client.say(server_msg())
